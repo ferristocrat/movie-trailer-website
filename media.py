aeab269f30ed255a854a601324154afd10601e6e
ferristocrat <michael@ferristocrat.com>
2015-06-18 22:57:04 -0400

    Added missing argument (movie_year) to __init__

--- media.py
+++ media.py
@@ -10,12 +10,13 @@ class Movie():
 		trailer_youtube_url:  A string that contains the youtube url to the movie's trailer
 	"""
 	
-	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, movie_year):
 		"""initializes the class Movie with certain characteristics"""
 		self.title = movie_title
 		self.storyline = movie_storyline
 		self.poster_image_url = poster_image
-		self.trailer_youtube_url =trailer_youtube
+		self.trailer_youtube_url = trailer_youtube
+		self.year = movie_year
 
 	def show_trailer(self):
 		"""show a trailer using the youtube url defined in __init__"""
